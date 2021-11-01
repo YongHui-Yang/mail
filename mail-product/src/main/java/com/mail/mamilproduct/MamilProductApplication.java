8a6753dab0e7e4026442b06d2e96373783844b5d
YongHui-Yang <66684871+yonghui-yang@users.noreply.github.com>
2021-11-01 16:27:19 +0800

    Initial commit 2021年11月1日 19点51分 第一次提交 测试GitHub

---
+++ mail-product/src/main/java/com/mail/mamilproduct/MamilProductApplication.java
@@ -0,0 +1,13 @@
+package com.mail.mamilproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamilProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MamilProductApplication.class, args);
+    }
+
+}
---
+++ mail-product/src/test/java/com/mail/mamilproduct/MamilProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mail.mamilproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamilProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
